2e7c20d54e3a14895f7db9998296ba34ac55eacb
Lucas <lucas.silva@agencian1.com.br>
2023-07-09 22:55:01 -0300

    save

--- tailwindcss-study/src/App.tsx
+++ tailwindcss-study/src/App.tsx
@@ -1,6 +1,15 @@
+import OrderComponent from "./components/OrderComponent";
+
 const App = ()=>{
   return(
-    <div className="text-3xl text-modify-green">Apenas um teste!</div>
+    <>
+      <div>
+        <div className="text-3xl text-modify-green bg-blue-600 w-1/3 h-24">1</div>
+        <div className="text-3xl text-modify-green bg-blue-600 w-1/2 h-24">2</div>
+        <div className="text-3xl text-modify-green bg-blue-600 w-screen h-24">3</div>
+      </div>
+      <OrderComponent />
+    </>
   );
 }
 
---
+++ tailwindcss-study/src/components/OrderComponent.tsx
@@ -0,0 +1,9 @@
+const OrderComponent = ()=>{
+    return(
+        <div className="h-screen w-screen">
+            <p className="">Apenas um texto para testes de tailwind</p>
+        </div>
+    );
+}
+
+export default OrderComponent;
